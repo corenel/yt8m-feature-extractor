f1a2cb57f5b46e2e87e3cb6e8e9ab4dd47cb8795
corenel <xxdsox@gmail.com>
2017-08-25 20:36:13 +0800

    extract feature of 2048 dims from inception v3 successfully.

--- misc/utils.py
+++ misc/utils.py
@@ -4,8 +4,15 @@ import torch
 from torch.autograd import Variable
 
 
-def make_variable(tensor, volatile=False):
+def make_variable(tensor):
     """Convert Tensor to Variable."""
     if torch.cuda.is_available():
         tensor = tensor.cuda()
-    return Variable(tensor, volatile=volatile)
+    return Variable(tensor)
+
+
+def make_cuda(model):
+    """Use CUDA if available."""
+    if torch.cuda.is_available():
+        model = model.cuda()
+    return model
